802445f78e57adf444effe32516b37f643d74ada
Pratyush Bagaria <prats13.live@gmail.com>
2018-11-28 09:43:23 -0600

    Code Files Pushed

---
+++ README.md
@@ -0,0 +1,35 @@
+## tack-it-up! [A MEAN Stack App]
+
+### About the project
+`tack-it-up` is a web application built on MEAN Stack viz. MongoDB, Express.js, Angular.js and Node.js. This app is basically a note-taking app which supports secure sign-up and login/logout features and all the notes/posts by the user are publicly readable by any visitor to the website. However functionality is implemented as such that the note/post can be modified or removed by the user who created it. Errors are handled efficiently while password encryption is in place to manage user accounts. For styling, Angular Material is used extensively.
+
+### Changes required before building the project
+1. Rename the folder to `mean-project` after cloning and unzipping
+2. Create a cluster on mongodb atlas (cloud db of mongodb), a database named "mean-project" in that cluster and two collections named "posts" in that database. Make sure to IP whitelist for all IP address for this cluster
+3. Copy the SRV Connection String for this cluster which should something like this: `mongodb+srv://prats:<PASSWORD>@cluster0-gitbh.mongodb.net/test?retryWrites=true`
+4. Remove `?retryWrites=true` and in place of `<PASSWORD>` type-in `" + process.env.DB_PSWRD + "`, simultaneously add this `<PASSWORD>` in `nodemon.json` available in `src` folder while replacing the contents for `DB_PSWRD` key there
+5. Navigate to backend/app.js file and replace this path in line 13
+
+
+### Execution steps to build and run from command line
+1. issue `ng serve` to build or compile the code on terminal window. Do not close this terminal window.
+2. issue `npm run start:server` in another terminal window to establish connection to the database.
+3. navigate to http://localhost:4200/ on your browser to see the app runnning.
+
+### External packages required
+
+ - bcryptjs : 2.4.3
+ - body-parser : 1.18.3
+ - express : 4.16.4
+ - jsonwebtoken : 8.4.0
+ - mongoose : 5.3.13
+ - mongoose-unique-validator : 2.0.2
+ - multer : 1.4.1
+ - rxjs : 6.3.3
+
+
+### Demo Link & Screenshot
+
+The app is deployed on AWS using its S3 and Elastic Beanstalk service. [Click here](http://tack-it-up.s3-website-us-east-1.amazonaws.com) to give it a try. Application's `logged-in state` is shown below:
+
+![](tack-it-up.png)
---
+++ backend/controllers/posts.js
@@ -0,0 +1,115 @@
+const Post = require("../models/post");
+
+exports.createPost = (req, res, next) => {
+  const url = req.protocol + "://" + req.get("host");
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: url + "/images/" + req.file.filename,
+    creator: req.userData.userId
+  });
+  post
+    .save()
+    .then(createdPost => {
+      res.status(201).json({
+        message: "Created Successfully!",
+        post: {
+          ...createdPost,
+          id: createdPost._id
+        }
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Creation Failed!"
+      });
+    });
+};
+
+exports.updatePost = (req, res, next) => {
+  let imagePath = req.body.imagePath;
+  if (req.file) {
+    const url = req.protocol + "://" + req.get("host");
+    imagePath = url + "/images/" + req.file.filename;
+  }
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    imagePath: imagePath,
+    creator: req.userData.userId
+  });
+  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+    .then(result => {
+      if (result.n > 0) {
+        res.status(200).json({ message: "Update Successful!" });
+      } else {
+        res.status(401).json({ message: "Not Authorized!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Update Failed!"
+      });
+    });
+};
+
+exports.getPosts = (req, res, next) => {
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  let fetchedPosts;
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  postQuery
+    .then(documents => {
+      fetchedPosts = documents;
+      return Post.countDocuments();
+    })
+    .then(count => {
+      res.status(200).json({
+        message: "Fetched Successfully!",
+        posts: fetchedPosts,
+        maxPosts: count
+      });
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetch Failed!"
+      });
+    });
+};
+
+exports.getPost = (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).json({ message: "Not Found!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Fetch Failed!"
+      });
+    });
+};
+
+exports.deletePost = (req, res, next) => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
+      console.log(result);
+      if (result.n > 0) {
+        res.status(200).json({ message: "Deleted Successfully!" });
+      } else {
+        res.status(401).json({ message: "Deletion Not Allowed!" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: "Deletion Failed!"
+      });
+    });
+};
